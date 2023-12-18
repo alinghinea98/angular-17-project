0290d1649a743013fe299e00a6b8bb9e6bc0778c
Alin Ghinea <alinghinea98@yahoo.com>
2023-12-18 21:05:28 +0200

    add learn more design and slider

--- src/app/components/learn-more/learn-more.component.ts
+++ src/app/components/learn-more/learn-more.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { register } from 'swiper/element/bundle';
+
+register();
 
 @Component({
   selector: 'app-learn-more',
   standalone: true,
   imports: [CommonModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './learn-more.component.html',
   styleUrl: './learn-more.component.scss',
   host: { class: 'component-container' },
